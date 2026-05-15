37aff57b6789361a0a3a1ddd4e0c1586bc0aad97
Caio Massaharu Damaceno Fumiya <31969541+caiofumiya@users.noreply.github.com>
2026-05-15 16:13:15 -0300

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        s = s.lower()
+        s = re.sub(r'[^a-zA-Z0-9]', '', s)
+        
+        
+        print(s)
+
+        i = 0
+        j = len(s)-1
+
+        if not s:
+            return True
+
+        while i != j:
+
+            if s[i] != s[j]:
+                return False
+            if j == i + 1:
+                return True
+            i += 1
+            j -= 1
+
+        return True
